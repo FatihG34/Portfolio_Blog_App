f552355f93fc8698af67f7c266602163d6d143d0
FatihG34 <fthgnydn34@gmail.com>
2022-08-30 23:35:54 +0300

    router modified

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from '../components/navbar/Navbar'
+import Dashboard from '../pages/dashboard/Dashboard'
+import Detail from '../pages/detail/Detail'
+import Register from '../pages/register/Register'
+import Signin from '../pages/signin/Signin'
 
 const AppRouter = () => {
     return (
-        <div>AppRouter</div>
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/detail' element={<Detail />} />
+                <Route path='/Register' element={<Register />} />
+                <Route path='/signin' element={<Signin />} />
+
+            </Routes>
+        </BrowserRouter>
     )
 }
 
